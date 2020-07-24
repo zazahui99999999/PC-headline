8574087fec1240488587e6eee6f575df9c2c4057
zazahui99999999 <2106564339@qq.com>
2020-07-24 16:29:19 +0800

    初始化项目-整理目录、准备资源

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入第三方包element-ui框架
-import 'element-ui/lib/theme-chalk/index' // 引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css' // 引入element-ui样式
+import '@/styles/index.less' // 引入全局公共样式
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册element-ui框架
 new Vue({
